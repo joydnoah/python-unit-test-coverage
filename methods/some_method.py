06785f934691b100aa2e78b61c67090055f8b534
Sebastian <ingesql1992@hotmail.com>
2020-12-28 16:40:30 -0500

    Testing coverage

---
+++ methods/some_method.py
@@ -0,0 +1,14 @@
+def method_for_testing(arg1, arg2):
+    return abs(arg1 - arg2)
+
+
+def method_for_testing2(arg1, arg2):
+    return -abs(arg1 - arg2)
+
+
+def method_for_testing3(arg1, arg2):
+    return abs(arg1 + arg2)
+
+
+def method_for_testing4(arg1, arg2):
+    return -abs(arg1 + arg2)
---
+++ tests/method_test.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+from methods import some_method
+
+
+class TestMethod(TestCase):
+    def test_one(self):
+        result = some_method.method_for_testing(1, 2)
+        self.assertEqual(1, result)
+        result = some_method.method_for_testing(2, 1)
+        self.assertEqual(1, result)
