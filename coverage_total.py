99a3b5f6bbfcedce4a1eb1f2ef7ebc083c85a71e
Sebastian <ingesql1992@hotmail.com>
2020-12-28 19:08:37 -0500

    Added coverage min

--- coverage_total.py
+++ coverage_total.py
@@ -1,9 +1,37 @@
-import coverage
 import os
+import sys
+import json
+import coverage
+import github
+
+def get_color(total):
+    if total > 80:
+        return "brightgreen"
+    if total > 30 and total < 80:
+        return "orange"
+    return "red"
+
 cov = coverage.coverage()
 cov.load()
 
 with open(os.devnull, "w") as f:
     total = cov.report(file=f)
 
-os.environ["COVERAGE"] = str(total)
+API_TOKEN = sys.argv[1]
+GIST_ID = sys.argv[2]
+
+gh = github.Github(API_TOKEN)
+gist = gh.get_gist(GIST_ID)
+gist.edit(
+    description="new description",
+    files={
+        "coverage.json": github.InputFileContent(
+            content=json.dumps({
+                "schemaVersion":1,
+                "label": "coverage",
+                "message": f"{total}%",
+                "color": get_color(total)
+            })
+        )
+    },
+)
