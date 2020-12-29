195b1640dc2627e111714c784a89025ae0fe5787
Sebastian <ingesql1992@hotmail.com>
2020-12-28 19:12:51 -0500

    Added coverage min

--- tests/method_test.py
+++ tests/method_test.py
@@ -8,3 +8,9 @@ class TestMethod(TestCase):
         self.assertEqual(1, result)
         result = some_method.method_for_testing(2, 1)
         self.assertEqual(1, result)
+
+    def test_two(self):
+        result = some_method.method_for_testing2(1, 2)
+        self.assertEqual(-1, result)
+        result = some_method.method_for_testing2(2, 1)
+        self.assertEqual(-1, result)
